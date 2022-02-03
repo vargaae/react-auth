6bb12c7a55ce35846958a7636f7ff5de3efefa43
Andras Varga <vargaae@hotmail.com>
2022-02-03 02:45:24 +0100

    set up Registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,42 +1,70 @@
-import React, { Component } from 'react';
+import React, { SyntheticEvent, useState } from "react";
 
-export class Register extends Component {
-  render() {
-    return <form>
-    <h1 className="h3 mb-3 fw-normal">Please register</h1>
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    <div className="form-floating">
-      <input
-        type="text"
-        className="form-control"
-        id="floatingName"
-        placeholder="John Doe"
-      />
-      <label htmlFor="floatingName">Name</label>
-    </div>
-    <div className="form-floating">
-      <input
-        type="email"
-        className="form-control"
-        id="floatingInput"
-        placeholder="name@example.com"
-      />
-      <label htmlFor="floatingInput">Email address</label>
-    </div>
-    <div className="form-floating">
-      <input
-        type="password"
-        className="form-control"
-        id="floatingPassword"
-        placeholder="Password"
-      />
-      <label htmlFor="floatingPassword">Password</label>
-    </div>
-    <button className="w-100 btn btn-lg btn-primary" type="submit">
-      Register
-    </button>
-  </form>;
-  }
-}
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/api/register", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const content = await response.json();
+
+    console.log(content)
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          id="floatingName"
+          placeholder="John Doe"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="floatingName">Name</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="email"
+          className="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="floatingInput">Email address</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      <button className="w-100 btn btn-lg btn-primary" type="submit">
+        Register
+      </button>
+    </form>
+  );
+};
 
 export default Register;
